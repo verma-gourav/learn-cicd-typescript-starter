5419e6bbb5e6f71ce6cddad5263b0b7c7f2efecc
Gourav Verma <gouravv.contact@gmail.com>
2025-12-23 07:52:41 +0000

    fail test

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,14 +1,14 @@
 import { IncomingHttpHeaders } from "http";
 
 export function getAPIKey(headers: IncomingHttpHeaders): string | null {
-  const authHeader = headers["authorization"];
+  const authHeader = headers["header"];
 
   if (!authHeader) {
     return null;
   }
 
   const splitAuth = authHeader.split(" ");
-  if (splitAuth.length < 2 || splitAuth[0] !== "ApiiKey") {
+  if (splitAuth.length < 2 || splitAuth[0] !== "ApiKey") {
     return null;
   }
 
